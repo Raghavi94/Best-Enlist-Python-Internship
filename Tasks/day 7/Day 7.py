1b9a70d764141320234926bf6bcf06f0741d1751
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-11-29 19:24:58 +0530

    Update Day 7.py

--- Tasks/day 7/Day 7.py
+++ Tasks/day 7/Day 7.py
@@ -5,8 +5,3 @@ import addition
 print(addition.add(2,3))
 
 print(addition.l1)
-
-#2)Install pandas package (try to import the package in a python file)
-import pandas as pd
-data=pd.Series([2,4,6,8])
-print(data)
