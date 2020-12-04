19795b91eec2645935a94c00698323379fa2aefc
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-12-04 17:39:03 +0530

    Add files via upload

---
+++ Tasks/Day 12/Day 12.py
@@ -0,0 +1,16 @@
+#TASK-12:
+
+#Create a JSON of all object types and import the JSON into a SQL Database
+
+import json
+print('Json Dict:',json.dumps({"name": "Ravi", "age": 30,"Place":"Chennai"}))
+print('Json List:',json.dumps(["apple", "bananas"]))
+print('Json-tuple:',json.dumps(("apple", "bananas")))
+print('Json String:',json.dumps("hello"))
+print('Json Int:',json.dumps(10))
+print('Json Float:',json.dumps(31.56))
+print('Json True:',json.dumps(True))
+print('Json False:',json.dumps(False))
+print('Json none:',json.dumps(None))
+    
+
