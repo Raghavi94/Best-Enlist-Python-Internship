1dd7ad4a526091e47a5215828809773df3e58503
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-12-05 12:01:19 +0530

    Update Day 12.py

--- Tasks/Day 12/Day 12.py
+++ Tasks/Day 12/Day 12.py
@@ -3,14 +3,38 @@
 #Create a JSON of all object types and import the JSON into a SQL Database
 
 import json
-print('Json Dict:',json.dumps({"name": "Ravi", "age": 30,"Place":"Chennai"}))
-print('Json List:',json.dumps(["apple", "bananas"]))
-print('Json-tuple:',json.dumps(("apple", "bananas")))
-print('Json String:',json.dumps("hello"))
-print('Json Int:',json.dumps(10))
-print('Json Float:',json.dumps(31.56))
-print('Json True:',json.dumps(True))
-print('Json False:',json.dumps(False))
-print('Json none:',json.dumps(None))
+from pymongo import MongoClient
+myclient = MongoClient("mongodb://localhost:27017/")
+
+a={
+    "firstName":"Joe",
+    "lastName":"Jacky",
+    "age":20,
+    "hobbies":["Jogging","Swimming","Dancing"],
+    "Student":True,
+    "Average":70.23
+    },{
+    "firstName":"Milind",
+    "lastName":"Soman",
+    "age":19,
+    "hobbies":["Singing","Drawing","Writing poems"],
+    "Student":True,
+    "Average":84.57
+    }
+with open("doc.json","w") as f:
+    json.dump(a,f,indent=4)
+
+
+#from pymongo import MongoClient
+#myclient=MongoClient("mongodb://localhost:4000/")
+db=myclient["mongdb"]
+Collection=db["info"]
+with open("doc.json") as f:
+    data=json.load(f)
+if isinstance(data,list):
+    Collection.insert_many(data)
+else:
+    Collection.insert_one(data)
+
     
 
