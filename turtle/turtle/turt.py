ee55dd958a3a72feba9f4e8c52332c2f1ce99339
Raghavi Srinivasan <53093082+Raghavi94@users.noreply.github.com>
2020-12-07 18:09:43 +0530

    Add files via upload

---
+++ turtle/turtle/turt.py
@@ -0,0 +1,12 @@
+#My first turtle program
+#To create a square
+import turtle
+my_turtle=turtle.Turtle()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.color("red")
